aebee83ed02351917fe57265b8ab6710bb5f6509
Doppelhammer Christoph WKO Inhouse <christoph.doppelhammer@inhouse.wko.at>
2026-03-09 17:03:07 +0100

    remove pragma

--- ITest.cs
+++ ITest.cs
@@ -1,7 +1,5 @@
 ﻿namespace SonarPragmaWarningBugExample;
 
-#pragma warning disable S4023 // Sonar Extension removes this
 public interface ITest
-#pragma warning restore S4023 // Sonar Extension removes this
 {
 }
